71b39c6e26b3777906cb6d7b8030950253b069b5
Mado <slamt6390@gmail.com>
2026-07-20 19:12:01 +0300

    refactor: convert user variables to dictionary

--- hello.py
+++ hello.py
@@ -1,29 +1,31 @@
+user_data = {
+    'transactions' : [],
+    'deposit_count' : 0,
+    'withdraw_count' : 0,
+    'name_change_count' : 0,
+    'total_deposit' : 0,
+    'total_withdraw' : 0,
+    'name' : input("Please enter your name:\t"),
+    'age' : float(input("Please enter your age:\t")),
+    'balance' : float(input("Please enter your balacne:\t")),
+    'pin' : "mado1230",
+    'tries' : 3,
+    'userpin' : input("What is your password?:\t")
+}
 
-transactions = []
-deposit_count = 0
-withdraw_count = 0
-name_change_count = 0
-total_deposit = 0
-total_withdraw = 0
-name = input("Please enter your name:\t")
-age = float(input("Please enter your age:\t"))
-balance = float(input("Please enter your balacne:\t"))
-pin = "mado1230"
-tries = 3
-userpin = input("What is your password?:\t")
-while userpin != pin:
-    tries -= 1
-    if tries == 0:
+while user_data['userpin'] != user_data["pin"]:
+    user_data['tries'] -= 1
+    if user_data['tries'] == 0:
         print("Your account has been locked.")
         exit()
 
     print("Wrong PIN")
-    print(f"You have {tries} attempts remaining.")
+    print(f"You have {user_data['tries']} attempts remaining.")
 
-    userpin = input("What is your password?:\t")
+    user_data['userpin'] = input("What is your password?:\t")
 
 print("Correct PIN!")
-print(f"Welcome to our ATM program, {name.capitalize()}")
+print(f"Welcome to our ATM program, {user_data['name'].capitalize()}")
 while True:
     print("""
 ===== ATM =====
@@ -43,38 +45,38 @@ while True:
     match choice:
         case 1:
             print(f"""
-            Name : {name}
-            Age : {age}
-            Balance : {balance:.2f}
+            Name : {user_data['name']}
+            Age : {user_data['age']}
+            Balance : {user_data['balance']:.2f}
             """)
-            if age>=18:
+            if user_data['age']>=18:
                 print("Adult : True")
             else:
                 print("Adult : False")
-            transactions.append("Viewed Account Info")   
+            user_data["transactions"].append("Viewed Account Info")   
             
         case 2:
             depo = float(input("How much do you want to deposit?"))
             if depo>0:
-                balance += depo
-                print(f"Your balance is: {balance:.2f}")
-                transactions.append(f"Deposited ${depo:.2f}")
-                deposit_count += 1
-                total_deposit += depo
+                user_data['balance'] += depo
+                print(f"Your balance is: {user_data['balance']:.2f}")
+                user_data["transactions"].append(f"Deposited ${depo:.2f}")
+                user_data['deposit_count'] += 1
+                user_data['total_deposit'] += depo
             else:
                 print("Invalid amount")
            
         case 3:
             withdraw = float(input("How much?:\t"))
-            if withdraw>balance:
+            if withdraw>user_data['balance']:
                 print("Insufficient balance")
             else:
                 print("The amount has been successfully withdrawn.")
-                balance -= withdraw
-                print(f"Your balance now is: {balance:.2f}")
-                transactions.append(f"Withdrew ${withdraw:.2f}")
-                withdraw_count += 1
-                total_withdraw += withdraw
+                user_data['balance'] -= withdraw
+                print(f"Your balance now is: {user_data['balance']:.2f}")
+                user_data["transactions"].append(f"Withdrew ${withdraw:.2f}")
+                user_data['withdraw_count'] += 1
+                user_data['total_withdraw'] += withdraw
         case 4:
             newname = input("What is your new name?:\t").lower()
             if len(newname)>12:
@@ -85,46 +87,46 @@ while True:
                 print("Username must not contain digits")
                 
             else:
-                name = newname
+                user_data['name'] = newname
 
-                print(f"Username updated successfully to {name.capitalize()}")
-                transactions.append(f"Changed name to {newname}")
-                name_change_count += 1
+                print(f"Username updated successfully to {user_data['name'].capitalize()}")
+                user_data["transactions"].append(f"Changed name to {newname}")
+                user_data['name_change_count'] += 1
                 
         case 5:
             
-            if len(transactions) == 0:
+            if len(user_data["transactions"]) == 0:
                 print("No transactions yet.")
             else:
                 print("Transaction History:")
-                for transaction in transactions:
+                for transaction in user_data["transactions"]:
                     print(transaction)
         case 6:
-            if transactions:
-               transactions.pop()
+            if user_data["transactions"]:
+               user_data["transactions"].pop()
                print("Last transaction deleted!")
             else:
                 print("No transactions to delete.")
            
         case 7:
-            transactions.clear()
+            user_data["transactions"].clear()
             print("History is already cleaned!")
         
         case 8:
             print(f"""
             ===== Statistics =====
 
-Balance : ${balance:.2f}
-Transactions : {len(transactions)}
-Deposits : {deposit_count}
-Withdrawals : {withdraw_count}
- Username Changes : {name_change_count}
- Total Deposited : ${total_deposit:.2f}
- Total Withdrawn : ${total_withdraw:.2f}
+Balance : ${user_data['balance']:.2f}
+Transactions : {len(user_data["transactions"])}
+Deposits : {user_data['deposit_count']}
+Withdrawals : {user_data['withdraw_count']}
+ Username Changes : {user_data['name_change_count']}
+ Total Deposited : ${user_data['total_deposit']:.2f}
+ Total Withdrawn : ${user_data['total_withdraw']:.2f}
             """)
 
         case 9:
-            print(f"Thank you for using our ATM, {name.capitalize()}!")
+            print(f"Thank you for using our ATM, {user_data['name'].capitalize()}!")
             break
         case _:
             print("Please enter one number of below")
