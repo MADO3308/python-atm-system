6944cdf656d097b325aa503c51af0a9e2f153bb6
Mado <slamt6390@gmail.com>
2026-07-27 15:46:07 +0300

    refactor: organize ATM operations into functions

--- hello.py
+++ hello.py
@@ -13,6 +13,122 @@ user_data = {
     'userpin' : input("What is your password?:\t"),
     
 }
+def showaccountinfo():
+     print(f"""
+           Name : {user_data['name']}
+           Age : {user_data['age']}
+           Balance : {user_data['balance']:.2f}
+           """)
+     if user_data['age']>=18:
+        print("Adult : True")
+     else:
+         print("Adult : False")
+     user_data["transactions"].append("Viewed Account Info")
+def deposit():
+    depo = float(input("How much do you want to deposit?"))
+    if depo>0:
+        user_data['balance'] += depo
+        print(f"Your balance is: {user_data['balance']:.2f}")
+        user_data["transactions"].append(f"Deposited ${depo:.2f}")
+        user_data['deposit_count'] += 1
+        user_data['total_deposit'] += depo
+    else:
+        print("Invalid amount")
+def withdraw():
+    withdraw = float(input("How much?:\t"))
+    if withdraw <= 0:
+        print("Invalid amount.")
+        
+    elif withdraw>user_data['balance']:
+        print("Insufficient balance")
+    else:
+        print("The amount has been successfully withdrawn.")
+        user_data['balance'] -= withdraw
+        print(f"Your balance now is: {user_data['balance']:.2f}")
+        user_data["transactions"].append(f"Withdrew ${withdraw:.2f}")
+        user_data['withdraw_count'] += 1
+        user_data['total_withdraw'] += withdraw
+def changeusername():
+    newname = input("What is your new name?:\t").lower()
+
+    if len(newname) > 12:
+        print("Please enter 12 characters or fewer.")
+
+    elif " " in newname:
+        print("Please remove the space!")
+
+    elif not newname.isalpha():
+        print("Username must not contain digits")
+
+    else:
+        user_data['name'] = newname
+
+        print(
+            f"Username updated successfully to "
+            f"{user_data['name'].capitalize()}"
+        )
+
+        user_data["transactions"].append(
+            f"Changed name to {newname}"
+        )
+
+        user_data['name_change_count'] += 1
+def show_tran_history():
+    if len(user_data["transactions"]) == 0:
+        print("No transactions yet.")
+    else:
+        print("Transaction History:")
+        for transaction in user_data["transactions"]:
+            print(transaction)
+def del_last_tran():
+    if user_data["transactions"]:
+        user_data["transactions"].pop()
+        print("Last transaction deleted!")
+    else:
+        print("No transactions to delete.")
+def clear_tran_history():
+    user_data["transactions"].clear()
+    print("History is already cleaned!")
+def statistics():
+    print(f"""
+            ===== Statistics =====
+
+    Balance : ${user_data['balance']:.2f}
+    Transactions : {len(user_data["transactions"])}
+    Deposits : {user_data['deposit_count']}
+    Withdrawals : {user_data['withdraw_count']}
+    Username Changes : {user_data['name_change_count']}
+    Total Deposited : ${user_data['total_deposit']:.2f}
+    Total Withdrawn : ${user_data['total_withdraw']:.2f}
+            """)
+def change_pass():
+    old_pass = input("What is your old password?:\t")
+    while old_pass != user_data["pin"]:
+        user_data['tries'] -= 1
+        if user_data['tries'] == 0:
+            print("Your account has been locked.")
+            exit()
+        print("Wrong PIN")
+        print(
+            f"You have {user_data['tries']} attempts remaining."
+        )
+        old_pass = input("What is your old password?:\t")
+    print("Correct PIN!")
+    new_pass = input("What is your new password?:\t")
+    while new_pass == old_pass:
+        print("Please enter a different password.")
+        new_pass = input("What is your new password?:\t")
+    user_data['pin'] = new_pass
+    user_data["transactions"].append(
+        "Changed password"
+    )
+    print("PIN changed successfully!")
+
+    user_data['tries'] = 3
+def exit_program():
+    print(f"Thank you for using our ATM, {user_data['name'].capitalize()}!")
+def invalid_choice():
+    print("Please enter one number of below")
 
 while user_data['userpin'] != user_data["pin"]:
     user_data['tries'] -= 1
@@ -27,6 +143,7 @@ while user_data['userpin'] != user_data["pin"]:
 
 print("Correct PIN!")
 print(f"Welcome to our ATM program, {user_data['name'].capitalize()}")
+    
 while True:
     print("""
 ===== ATM =====
@@ -46,111 +163,30 @@ while True:
 
     match choice:
         case 1:
-            print(f"""
-            Name : {user_data['name']}
-            Age : {user_data['age']}
-            Balance : {user_data['balance']:.2f}
-            """)
-            if user_data['age']>=18:
-                print("Adult : True")
-            else:
-                print("Adult : False")
-            user_data["transactions"].append("Viewed Account Info")   
+            showaccountinfo()
             
         case 2:
-            depo = float(input("How much do you want to deposit?"))
-            if depo>0:
-                user_data['balance'] += depo
-                print(f"Your balance is: {user_data['balance']:.2f}")
-                user_data["transactions"].append(f"Deposited ${depo:.2f}")
-                user_data['deposit_count'] += 1
-                user_data['total_deposit'] += depo
-            else:
-                print("Invalid amount")
+            deposit()
            
         case 3:
-            withdraw = float(input("How much?:\t"))
-            if withdraw>user_data['balance']:
-                print("Insufficient balance")
-            else:
-                print("The amount has been successfully withdrawn.")
-                user_data['balance'] -= withdraw
-                print(f"Your balance now is: {user_data['balance']:.2f}")
-                user_data["transactions"].append(f"Withdrew ${withdraw:.2f}")
-                user_data['withdraw_count'] += 1
-                user_data['total_withdraw'] += withdraw
+            withdraw()
         case 4:
-            newname = input("What is your new name?:\t").lower()
-            if len(newname)>12:
-                print("Please enter 12 characters or fewer.")
-            elif " "in newname:
-                print("Please remove the space!")
-            elif not newname.isalpha():
-                print("Username must not contain digits")
-                
-            else:
-                user_data['name'] = newname
-
-                print(f"Username updated successfully to {user_data['name'].capitalize()}")
-                user_data["transactions"].append(f"Changed name to {newname}")
-                user_data['name_change_count'] += 1
-                
+            changeusername()
         case 5:
-            
-            if len(user_data["transactions"]) == 0:
-                print("No transactions yet.")
-            else:
-                print("Transaction History:")
-                for transaction in user_data["transactions"]:
-                    print(transaction)
+            show_tran_history()
         case 6:
-            if user_data["transactions"]:
-               user_data["transactions"].pop()
-               print("Last transaction deleted!")
-            else:
-                print("No transactions to delete.")
-           
+            del_last_tran()
         case 7:
-            user_data["transactions"].clear()
-            print("History is already cleaned!")
+            clear_tran_history()
         
         case 8:
-            print(f"""
-            ===== Statistics =====
-
-Balance : ${user_data['balance']:.2f}
-Transactions : {len(user_data["transactions"])}
-Deposits : {user_data['deposit_count']}
-Withdrawals : {user_data['withdraw_count']}
- Username Changes : {user_data['name_change_count']}
- Total Deposited : ${user_data['total_deposit']:.2f}
- Total Withdrawn : ${user_data['total_withdraw']:.2f}
-            """)
+            statistics()
 
         case 9:
-            old_pass = input("What is your old password?:\t")
-            while old_pass != user_data["pin"]:
-                user_data['tries'] -= 1
-                if user_data['tries'] == 0:
-                    print("Your account has been locked.")
-                    exit()
-                else:
-                    print("Wrong PIN")
-                    print(f"You have {user_data['tries']} attempts remaining.")
-                    old_pass = input("What is your old password?:\t")
-
-            print("Correct PIN!")
-            new_pass = input("What is your new password?:\t")
-            while new_pass == old_pass:
-                print("Please Enter differenet password")
-                new_pass = input("What is your new password?:\t")
-            user_data['pin'] = new_pass
-            user_data["transactions"].append(f"Changed password")
-            print("PIN changed successfully!")
-            user_data['tries'] = 3
+            change_pass()
         
         case 10:
-            print(f"Thank you for using our ATM, {user_data['name'].capitalize()}!")
+            exit_program()
             break
         case _:
-            print("Please enter one number of below")
+            invalid_choice()
